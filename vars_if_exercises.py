c4b8dd9a2b4e59591ca8b199c8fe184ce0209cb7
Alphonce Liguori <orenyalphy256@gmail.com>
2025-09-11 22:47:48 +0300

    Day5 variables, input, if, statements

---
+++ vars_if_exercises.py
@@ -0,0 +1,8 @@
+name = input("Alphonce Liguori")
+age = input(27)
+print("Hello Alphonce Liguori, you are 27 years old")
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+    
